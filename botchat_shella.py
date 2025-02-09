240654801ad9a10e9caecb9f1bb4cba69b17c35a
shellatheresyapandiangan <80011501+shellatheresyapandiangan@users.noreply.github.com>
2025-02-10 00:07:15 +0700

    botchat_shella.py

--- botchat_shella.py
+++ botchat_shella.py
@@ -107,7 +107,7 @@ st.sidebar.markdown("""
 """, unsafe_allow_html=True)
 
 # Menampilkan gambar kecil di bawah About This App
-st.sidebar.image("image.png", caption="Shella Pandiangan", width=80)
+st.sidebar.image("shel.jpg, caption="Shella Pandiangan", width=80)
 
 # Input pengguna
 txt_container = st.container()
